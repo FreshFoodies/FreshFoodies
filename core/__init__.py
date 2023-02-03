b21cb67e4d6a717a194480391424906332181f6b
Arjun Srivastava <arjunsrivastava01@gmail.com>
2023-02-03 06:35:09 -0800

    small fixes ;

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from core.food.models import Category
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
