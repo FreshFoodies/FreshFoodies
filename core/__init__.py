4e521c35166ddd6c1f15937a1f396da7e267b135
Arjun Srivastava <arjunsrivastava01@gmail.com>
2023-02-03 14:55:44 -0800

    working on mongodb setup

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from config import Configuration
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from pymongo.collection import Collection
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
