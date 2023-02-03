825256c41d0c8a415d1a0b6ebced1894eb69e453
David Lee <thedavl2001@gmail.com>
2023-02-03 13:15:27 -0800

    add skeleton for receipt processing

--- core/__init__.py
+++ core/__init__.py
@@ -11,4 +11,6 @@ migrate = Migrate(app, db)
 # blueprint for non-authentication parts of the app
 from .food import food as food_blueprint
 app.register_blueprint(food_blueprint)
+from .receipt import receipt as receipt_blueprint
+app.register_blueprint(receipt_blueprint)
 from core import views, models  
